b4c75c072f46b089b6212dee4c14c06337a7cb8b
Ekaterina Terenicheva <Eventcpa@gmail.com>
2020-11-19 17:03:26 +0600

    all project + rename field

---
+++ Assets/Scripts/GenerateEnemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _spawnEnemy;
+    [SerializeField] private Transform _path;
+    [SerializeField] private float _waitTime;
+
+    private Transform[] _points;
+
+    private void Start()
+    {
+        _points = new Transform[_path.childCount];
+
+        for (int i = 0; i < _path.childCount; i++)
+        {
+            _points[i] = _path.GetChild(i);
+        }
+        StartCoroutine(GenerateEnemies());
+    }
+
+    private IEnumerator GenerateEnemies()
+    {
+        var time = new WaitForSeconds(_waitTime);
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            Instantiate(_spawnEnemy, new Vector3(_points[i].position.x, _points[i].position.y, 0), Quaternion.identity);
+            yield return time;
+        }
+    }
+}
